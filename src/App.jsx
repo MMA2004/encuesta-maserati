9ede093872ed3067d82adf011001e834da95f2df
Mateo Monroy <monroymateo1@gmail.com>
2026-02-09 17:19:58 -0500

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MaseratiSurvey from "./pages/MaseratiSurvey.jsx";
+
+function Inicio() {
+    const navigate = useNavigate();
+
+    const irAEncuesta = () => {
+        // puedes cambiar el id por el que necesites
+        navigate("/maserati/123");
+    };
+
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>Inicio</h1>
+            <button onClick={irAEncuesta}>
+                Ir a la encuesta
+            </button>
+        </div>
+    );
+}
+
 export default function App() {
     return (
 
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Inicio />} />
                 <Route path="/maserati/:surveyId" element={<MaseratiSurvey />} />
             </Routes>
         </BrowserRouter>
