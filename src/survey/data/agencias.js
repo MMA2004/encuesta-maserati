3da4e4804bf63a40136bc3d2b0aeef8451d67518
Mateo Monroy <monroymateo1@gmail.com>
2026-04-08 17:39:23 -0500

    add: initial agencies data file for survey

---
+++ src/survey/data/agencias.js
@@ -0,0 +1,32 @@
+export const AGENCIAS_DATA = {
+    "Santa Fe": [
+        "Raymundo Aguirre",
+        "Leonardo Rivera",
+        "David Cortés",
+        "Diana Gonzalez",
+        "Miguel Vidal",
+        "Cristina Sosa",
+        "Maximiliano Yañez"
+    ],
+    "Insurgentes": [
+        "Raymundo Aguirre",
+        "Leonardo Rivera",
+        "David Cortés",
+        "Diana Gonzalez",
+        "Miguel Vidal",
+        "Cristina Sosa",
+        "Maximiliano Yañez"
+    ],
+    "Guadalajara": [
+        "Omar Nuño",
+        "Miguel Arroyo",
+        "Fernando Ramos"
+    ],
+    "Monterrey": [
+        "Alan Estrada",
+        "Brenda Martínez"
+    ]
+};
+
+// Esta lista se genera automáticamente a partir de las llaves del objeto de arriba
+export const LISTA_AGENCIAS = Object.keys(AGENCIAS_DATA);
